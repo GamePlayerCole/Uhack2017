c91416912f336949352a0e2714d2e1a447abab1b
Macbook <nalinsuri@gmail.com>
2017-12-02 23:22:17 -0500

    Modularize code into reusable functions

--- clientSide.js
+++ clientSide.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
+	console.log(restaurantsList);
 	$("#new").on("click", function(){
-		getLocation();
+		if (typeof restaurantsList === 'undefined' || restaurantsList === null || restaurantsList.length == 0) {
+			getLocation();
+		} else {
+			getRandomRestaurant();
+		}
+		
 	});
 
 });
 
-var restaurant;
+var restaurantsList;
 
 function getRandomInt(min, max) {
 		min = Math.ceil(min);
@@ -13,30 +19,43 @@ function getRandomInt(min, max) {
 		return Math.floor(Math.random() * (max - min)) + min; 
 }
 
-function getRandomRestaurant(lat,longitude){
+function setHtmlAttributes(name, href, addy, phone, imgUrl, gmapLink) {
+	$("#name").html(name);
+	$("#name").attr("href", href);
+	$("#address").html(addy.join(", "));
+	$("#phone-number").html(phone);
+	$("#pic").attr("src", imgUrl);
+	$("#gmaps-link").html("Google Maps Link");		
+	$("#gmaps-link").attr("href", gmapLink);
+}
+
+function getRestaurantsList(lat,longitude){
 
 	var restaurantRequest = new XMLHttpRequest();
 	restaurantRequest.open("GET", "http://54.148.9.45:3000/yelp?latitude=" + lat+"&longitude="+longitude, true);
 
 	restaurantRequest.onload = function(e){
-		console.log(restaurantRequest.responseText);
-		var restaurantsList = JSON.parse(restaurantRequest.responseText);
-		var randomNum = getRandomInt(0,restaurantsList.restaurants.length);
-		var randomRestaurant = restaurantsList.restaurants[randomNum];
-		restaurant = randomRestaurant;
-		var href = randomRestaurant.url;
-		var name = randomRestaurant.name;
-		var addy = randomRestaurant.location.display_address;
-		var phone = randomRestaurant.display_phone;
-		var imgUrl = randomRestaurant.image_url;
-		$("#name").html(name);
-		$("#name").attr("href", href);
-		$("#address").html(addy.join(", "));
-		$("#phone-number").html(phone);
-		$("#pic").attr("src", imgUrl);
+		console.log("Sending request to server");
+		restaurantsList = JSON.parse(restaurantRequest.responseText);
+		getRandomRestaurant(lat, longitude);
+		$("#new").html("Nope, Gimme Another!");
 	};
 
 	restaurantRequest.send();
+}
+
+function getRandomRestaurant(lat, long) {
+	console.log("Pulling random restaurant from list");
+	var randomNum = getRandomInt(0,restaurantsList.restaurants.length);
+	var randomRestaurant = restaurantsList.restaurants[randomNum];
+	var href = randomRestaurant.url;
+	var name = randomRestaurant.name;
+	var addy = randomRestaurant.location.display_address;
+	var phone = randomRestaurant.display_phone;
+	var imgUrl = randomRestaurant.image_url;
+	var gmapLink = createDirectionsLink([lat, long], addy);
+	console.log(name, addy, href, phone, imgUrl, gmapLink);
+	setHtmlAttributes(name, href, addy, phone, imgUrl, gmapLink);
 }
 
 function getLocation() {
@@ -48,5 +67,5 @@ function getLocation() {
 }
 
 function showPosition(position) {
-	getRandomRestaurant(position.coords.latitude, position.coords.longitude);
+	getRestaurantsList(position.coords.latitude, position.coords.longitude);
 }
