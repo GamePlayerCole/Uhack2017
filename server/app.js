358017885de6bf13c38280571f62806c83ca1921
Macbook <nalinsuri@gmail.com>
2017-12-07 18:46:32 -0500

    Add random offset to node api

--- server/app.js
+++ server/app.js
@@ -4,19 +4,16 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 const cors = require('cors');
 var fs = require('fs');
-// var https = require('https');
 
 const app = express();
+
+const CLIENT_ID = '';
+const CLIENT_SECRET = '';
+
 
 app.use(logger('dev'));
 app.use(cors());
 
-/*
-var options = {
-	key: fs.readFileSync("./keys/mockserver.key"),
-	cert: fs.readFileSync("./keys/mockserver.crt")
-}
-*/
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
@@ -27,17 +24,21 @@ app.get('/', (req, res) => {
 
 
 app.get('/yelp', function(req, res) {
-	axios.post('https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}')
+	axios.post('https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=' + CLIENT_ID + 
+		'&client_secret=' + CLIENT_SECRET)
 	  .then(response => {
 	  	var token = response['data']['access_token'];
+		const AuthStr = 'Bearer ' + token; 
 		var lat = req.param('latitude');
 		var long = req.param('longitude');
 		var radius = Math.floor(milesToMeters(req.param('radius')));
+		var offset = calcRandomOffset(900);
 
-		const AuthStr = 'Bearer ' + token; 
-		axios.get('https://api.yelp.com/v3/businesses/search?latitude=' + lat + '&longitude=' + long + '&radius=' + radius + '&limit=50', { headers: { Authorization: AuthStr } })
+		console.log(lat, long, radius, offset);
+
+		axios.get('https://api.yelp.com/v3/businesses/search?latitude=' + lat + '&longitude=' + long 
+			+ '&radius=' + radius + '&offset=' + offset + '&limit=50', { headers: { Authorization: AuthStr } })
 		 .then(restaurantsList => {
-		 	console.log(restaurantsList.data);
 		     return res.json({restaurants: restaurantsList.data.businesses});
 		  })
 		 .catch((error) => {
@@ -49,8 +50,8 @@ app.get('/yelp', function(req, res) {
 	  });
 });
 
-function milesToMeters(miles) { return miles * 1609.34 };
+function milesToMeters(miles) { return miles * 1609.34 }
+
+function calcRandomOffset(max) { return Math.floor(Math.random() * max) }
 
 app.listen(3000, () => console.log('Application on port 3000'));
-
-// Todo Radius, 
